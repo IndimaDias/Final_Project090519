dfd2d3528a41f04a68ea205d54ffeb62b2223a61
IndimaDias <indimad@yahoo.com>
2019-09-07 21:49:47 -0400

    Account creation form layout changes

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,19 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBCard,
+  MDBCardBody,
+  MDBView,
+  MDBMask,
+  MDBAnimation
+} from "mdbreact";
+
+
 import "./css/about.css";
 
 class About extends React.Component {
@@ -11,36 +24,17 @@ class About extends React.Component {
         <div className = "container-fluid test">
           <div className ="Row rowHeading">
               <div className = "Col center"> 
-                <h1 className ="text-light text-center"> Welcome to </h1>
-                <h1 className = "text-light text-center">DAD'S ORGANIZER</h1>
+                <p className ="text-light text-center heading1"> Login to </p>
+                <p className = "text-light text-center heading2"> BetterDad</p>
                 <Link to="/">
                     <button className= "btn btn-light btn-lg" style={{marginLeft:"45%"}}>Click to Login</button>
                </Link>
             </div>
           </div>
 
-        </Router>
+    
 
-        <MDBView>
-          <MDBMask className="d-flex justify-content-center align-items-center">
-            <MDBContainer>
-              <MDBRow>
-                <MDBAnimation
-                  type="fadeInLeft"
-                  delay=".3s"
-                  className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
-                >
-                  <h1 className="h1-responsive font-weight-bold">
-                    Login to BetterDad
-                  </h1>
-                  <hr className="hr-light" />
-                  <h6 className="mb-4">
-                    BetterDad is an app designed to help busy families schedule events,
-                    activities,playdates and more. All while allowing you to assign the tasks
-                    to members in the family as well as set up reminders for the event. 
-                  </h6>
-                </MDBAnimation>
-
+  
 
         </div>
       
